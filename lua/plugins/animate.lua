0408735a43320cc552fadc99075e43e68133e90e
Angelo Cicero <10821534+AngeloCicero@users.noreply.github.com>
2025-04-19 10:28:41 -0400

    plugin: animate dim in zen mode

--- lua/plugins/animate.lua
+++ lua/plugins/animate.lua
@@ -2,6 +2,11 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      dim = {
+        animate = {
+          enabled = false,
+        },
+      },
       indent = {
         animate = {
           enabled = false,
