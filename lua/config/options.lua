30282487ac6306716f05eeb6c27c48144aa30a9d
Angelo Cicero <10821534+AngeloCicero@users.noreply.github.com>
2025-05-04 11:35:43 -0400

    remap for zz when scroll

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,5 +6,9 @@ vim.g.root_spec = { "cwd" }
 vim.o.shell = "/usr/bin/fish"
 vim.opt.clipboard = "unnamedplus"
 
+-- Remaps
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
 -- Disable animations
 vim.g.snacks_animate = true
