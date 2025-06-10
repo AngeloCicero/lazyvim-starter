0dc43700afcdd45d9178cec23a36e8a9c039a285
Angelo Cicero <10821534+AngeloCicero@users.noreply.github.com>
2025-06-10 17:39:37 -0400

    python lsp & picker ignore

--- lua/plugins/lspConfig.lua
+++ lua/plugins/lspConfig.lua
@@ -1,6 +1,20 @@
 return {
-  "nvim-lspconfig",
+  "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      pyright = {
+        before_init = function(_, config)
+          local util = require("lspconfig.util")
+          local root_dir = util.root_pattern(".git", "pyproject.toml", "setup.py", "venv")(vim.fn.getcwd())
+          local venv_python = root_dir and root_dir .. "/venv/bin/python"
+          if vim.fn.executable(venv_python) == 1 then
+            config.settings = config.settings or {}
+            config.settings.python = config.settings.python or {}
+            config.settings.python.pythonPath = venv_python
+          end
+        end,
+      },
+    },
   },
 }
--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -4,7 +4,12 @@ return {
     opts = {
       files = {
         -- fd options to ignore common junk
-        fd_opts = [[--color=never --type f --hidden --exclude .git --exclude node_modules --exclude vendor]],
+        fd_opts = [[
+          --color=never --type f --hidden
+          --exclude .git --exclude node_modules --exclude vendor
+          --exclude venv --exclude __pycache__ --exclude .mypy_cache
+          --exclude .pytest_cache --exclude dist --exclude build
+        ]],
       },
     },
   },
