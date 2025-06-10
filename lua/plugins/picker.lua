10435ae295c7aa90160ed242b740591c242bbc88
Angelo Cicero <10821534+AngeloCicero@users.noreply.github.com>
2025-06-10 17:40:20 -0400

    python lsp & picker ignore

--- lua/plugins/picker.lua
+++ lua/plugins/picker.lua
@@ -3,6 +3,10 @@ return {
     "folke/snacks.nvim",
     opts = {
       picker = {
+        enabled = true,
+        -- completely ignore these folders
+        exclude = { "venv", "node_modules", "vendor" },
+
         sources = {
           files = {
             hidden = true,
