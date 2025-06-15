fe6478e6246c1612d11a0e537f73de34261552aa
Angelo Cicero <10821534+AngeloCicero@users.noreply.github.com>
2025-06-14 21:19:37 -0400

    add copilot

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,3 +9,11 @@ vim.opt.termguicolors = true
 
 -- Disable animations
 vim.g.snacks_animate = false
+
+-- CodeCompanion
+vim.keymap.set({ "n", "v" }, "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+-- Expand 'cc' into 'CodeCompanion' in the command line
+vim.cmd([[cab cc CodeCompanion]])
